10e7e24dbdca17c0a4334dfd8eef31468e87a833
Sabrina Yen-Ko <59457991+syenko@users.noreply.github.com>
2026-02-14 10:13:47 -0800

    Initial vite app w/ tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="w-full h-full flex flex-col items-center justify-center">
+            <div>Hello</div>
+        </div>
+    );
+}
+
+export default App;
